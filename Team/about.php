35c4464c6333eb06545cd0190c022f7e2255266f
011446032 <venkatesh.ds2u@gmail.com>
2016-12-06 18:59:18 -0800

    about,contact pages

---
+++ Team/about.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Uber Lyft Trip Planner</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Uber Lyft Trip Planner
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href= "myprofile.php">My Profile</a>
+                </li>
+                <li>
+                    <a href="signup.php">Sign Up</a>
+                </li>
+                <li>
+                    <a href="login.php">Login</a>
+                </li>
+                <li>
+                    <a href="planTrip.php">Plan Your Trip</a>
+                </li>
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+                <li>
+                    <a href="services.php">Services</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+                 <li>
+                    <a href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+        <div id="page-content-wrapper">
+
+        <h1>About Us</h1><br>
+
+        <h2>Trip Planner using Uber vs Lyft's Price Estimation</h2>
+        <br>
+        <br>
+        <ul>
+        <li><p>Our Company aims to provide an easy way to plan a trip for our customers</p></li>
+        <li><p>Our Mission is to provide very informative knowledge on the overall trip </p></li>
+        <li><p>We have Customers throughout the world</p></li>
+        
+
+        
+        </ul>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        
+
+        <center><img  src="images/logo.png" alt="Logo" style="width:200px;height:200px;"></img></center>
+        <b><p style="text-align: center"> Ashutosh Singh, Ashay Argal, Venkatesh Ds, Rishikesh </p></b>
+           
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+   
+</body>
+
+</html>
